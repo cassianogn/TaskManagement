f4023af195d5c9118d438f85b1ff1fcfe590290d
Cassiano Nunes <cassianogn@gmail.com>
2025-12-29 10:50:48 -0300

    feat: implement TaskItem and ITaskItemRepository classes

---
+++ TaskManagement.Api/TaskManagement/TaskManagement.Domain/TaskItems/ITaskItemRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.Domain.TaskItems
+{
+    public interface ITaskItemRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<TaskItem> CreateAsync(TaskItem taskItem, CancellationToken cancellationToken = default);
+        Task<TaskItem> UpdateAsync(TaskItem taskItem, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ TaskManagement.Api/TaskManagement/TaskManagement.Domain/TaskItems/TaskItem.cs
@@ -0,0 +1,31 @@
+﻿namespace TaskManagement.Domain.TaskItems
+{
+    public class TaskItem
+    {
+        private TaskItem()
+        {
+        }
+        public TaskItem(string title, DateTime createdAt)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            CreatedAt = createdAt;
+        }
+        public Guid Id { get; }
+        public string Title { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public DateTime CreatedAt { get; }
+        public DateTime? UpdatedAt { get; private set; }
+
+        public void ToggleStatus()
+        {
+            IsCompleted = !IsCompleted;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void Update(string title, string? description = null)
+        {
+            Title = title;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
