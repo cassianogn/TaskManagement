e36d2cac979a8d4029a51d0d415983aa594ea040
Cassiano Nunes <cassianogn@gmail.com>
2025-12-29 22:08:33 -0300

    feat: add TaskManagementDbContext and TaskItemRepository classes with initial setup

---
+++ TaskManagement.Api/TaskManagement/TaskManagement.Infrastructure/Data/TaskManagementDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.TaskItems;
+
+namespace TaskManagement.Infrastructure.Data
+{
+    public class TaskManagementDbContext : DbContext
+    {
+        public TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TaskItem>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).IsRequired();
+                entity.Property(e => e.IsCompleted).IsRequired();
+            });
+        }
+    }
+}
