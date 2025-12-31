652c0155a972b869ce4d3c1bbe6c62961556accd
Cassiano Nunes <cassianogn@gmail.com>
2025-12-31 13:12:36 -0300

    test: add integration test for toggling task item in TaskItemHandlersTest

--- TaskManagement.Api/TaskManagement/TaskManagement.Tests/TaskItemTests/TaskItemHandlersTest.cs
+++ TaskManagement.Api/TaskManagement/TaskManagement.Tests/TaskItemTests/TaskItemHandlersTest.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using TaskManagement.Application.Base.Handler;
 using TaskManagement.Application.TaskItems.Commands.AddTaskItem;
+using TaskManagement.Application.TaskItems.Commands.ToggleTaskItem;
 using TaskManagement.Application.TaskItems.Queries;
 using TaskManagement.Domain.TaskItems;
 using TaskManagement.Infrastructure;
@@ -51,9 +52,36 @@ namespace TaskManagement.Tests.TaskItemTests
         [Fact]
         public async Task HandleTaskItem_ShouldToggle_RunWIthSuccess()
         {
+            // Arrange
+            var queryHandler = new GetTaskItemsQueryHandler(_taskItemRepository);
+            var addCommand = new AddTaskItemCommand("Test Task for Toggle");
+            var addHandler = new AddTaskItemCommandHandler(_taskItemRepository);
+            // Act
+            HandlerResponse<Guid> result = await addHandler.HandleAsync(addCommand);
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccessful);
+         
+            // arrange
+            var toggleCommand = new ToggleTaskItemCommand(result.Response);
+            var toggleHandler = new ToggleTaskItemCommandHandler(_taskItemRepository);
+            Guid newTaskItemId = result.Response;
+            // Act
+            HandlerResponse toggleResult = await toggleHandler.HandleAsync(toggleCommand);
+            // Assert
+            Assert.NotNull(toggleResult);
+            Assert.True(toggleResult.IsSuccessful);
+            Assert.Null(toggleResult.ErrorMessage);
             
+            var finalQueryCommand = new GetTaskItemsQuery(addCommand.Title);
+            HandlerResponse<IReadOnlyCollection<GetTaskItemsQueryResult>> finalQueryResult = await queryHandler.HandleAsync(finalQueryCommand);
+            Assert.True(finalQueryResult.IsSuccessful);
+            Assert.NotNull(finalQueryResult);
+            Assert.NotNull(finalQueryResult.Response);
+            var toggledTAsk = finalQueryResult.Response.FirstOrDefault(taskItem => taskItem.Id == newTaskItemId);
+            Assert.NotNull(toggledTAsk);
+            Assert.True(toggledTAsk!.IsCompleted, "Task should be marked as completed, but does not.");
         }
-
 
         public void Dispose()
         {
