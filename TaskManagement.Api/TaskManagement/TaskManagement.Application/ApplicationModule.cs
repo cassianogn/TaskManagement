d6aad1ff35ed588b777e5326566abbfcbc5c47c4
Cassiano Nunes <cassianogn@gmail.com>
2025-12-31 14:08:33 -0300

    feat: add ApplicationModule to configure dependency injection for command handlers

---
+++ TaskManagement.Api/TaskManagement/TaskManagement.Application/ApplicationModule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.TaskItems.Commands.AddTaskItem;
+using TaskManagement.Application.TaskItems.Commands.ToggleTaskItem;
+using TaskManagement.Application.TaskItems.Queries;
+using TaskManagement.Domain.TaskItems;
+
+namespace TaskManagement.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<AddTaskItemCommandHandler>();
+            services.AddScoped<ToggleTaskItemCommandHandler>();
+            services.AddScoped<GetTaskItemsQueryHandler>();
+
+            return services;
+        }
+    }
+}
