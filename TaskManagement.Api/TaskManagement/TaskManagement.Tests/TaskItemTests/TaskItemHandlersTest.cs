d83920a788b03d664889252094fcc1c0dc93e616
Cassiano Nunes <cassianogn@gmail.com>
2025-12-31 10:54:13 -0300

    feat: enhance TaskItemHandlersTest to use ITaskItemRepository and add necessary infrastructure references

--- TaskManagement.Api/TaskManagement/TaskManagement.Tests/TaskItemTests/TaskItemHandlersTest.cs
+++ TaskManagement.Api/TaskManagement/TaskManagement.Tests/TaskItemTests/TaskItemHandlersTest.cs
@@ -1,21 +1,35 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using TaskManagement.Application.Base.Handler;
 using TaskManagement.Application.TaskItems.Commands.AddTaskItem;
 using TaskManagement.Application.TaskItems.Queries;
 using TaskManagement.Domain.TaskItems;
+using TaskManagement.Infrastructure;
 
 namespace TaskManagement.Tests.TaskItemTests
 {
-    public class TaskItemHandlersTest
+    public class TaskItemHandlersTest : IDisposable
     {
-        private readonly Mock<ITaskItemRepository> _taskItemRepositoryMock = new Mock<ITaskItemRepository>();
+        private readonly ITaskItemRepository _taskItemRepository;
+        private readonly ServiceProvider _serviceProvider;
+        private readonly IServiceScope _scope;
+
+        public TaskItemHandlersTest()
+        {
+            var taskItems = new List<TaskItem>();
+            _serviceProvider = new ServiceCollection().AddInfrastructure().BuildServiceProvider();
+            _scope = _serviceProvider.CreateScope();
+            _taskItemRepository = _scope.ServiceProvider.GetRequiredService<ITaskItemRepository>();
+
+        }
+
         [Fact]
         public async Task HandleTaskItem_CorrectlyProcessesValidTaskItemAsync()
         {
             // Arrange
             var addCommand = new AddTaskItemCommand("Test Task");
-            var addHandler = new AddTaskItemCommandHandler(_taskItemRepositoryMock.Object);
+            var addHandler = new AddTaskItemCommandHandler(_taskItemRepository);
 
             // Act
             var result = await addHandler.HandleAsync(addCommand);
@@ -24,7 +38,7 @@ namespace TaskManagement.Tests.TaskItemTests
 
             // arrange 
             var command = new GetTaskItemsQuery(addCommand.Title);
-            var handler = new GetTaskItemsQueryHandler(_taskItemRepositoryMock.Object);
+            var handler = new GetTaskItemsQueryHandler(_taskItemRepository);
 
             // Act
             HandlerResponse<IReadOnlyCollection<GetTaskItemsQueryResult>> queryResult = await handler.HandleAsync(command);
@@ -34,5 +48,12 @@ namespace TaskManagement.Tests.TaskItemTests
             Assert.True(queryResult.IsSuccessful);
             Assert.Contains(queryResult.Response, item => item.Title.Contains(addCommand.Title));
         }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
+            _serviceProvider.Dispose();
+        }
+
     }
 }
